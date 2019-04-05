061b0e731f937951d38e7a863c6b7735dd3cad06
Frank Bauer <frank.bauer@fau.de>
2019-04-05 15:51:45 +0200

    better naming

--- cLib/Property.h
+++ cLib/Property.h
@@ -1,20 +1,19 @@
 #ifndef __PROPERTY__
 #define __PROPERTY__
 
-template<class _Prop_t,
-         class _ObjClass_t>
+template<class PropertyType, class ParentObject>
 class Property
 {
-    typedef _Prop_t (_ObjClass_t::* _pmGet_t)() const;
+    typedef PropertyType (ParentObject::* propertyGetter)() const;
 
-    _ObjClass_t& m_objInstance;
-    _pmGet_t     m_pmGet;
+    ParentObject& m_objInstance;
+    propertyGetter     getterMethod;
 
 public:
-    Property(_ObjClass_t& objInstance, _pmGet_t pmGet)
-    :  m_objInstance(objInstance), m_pmGet(pmGet)
+    Property(ParentObject& objInstance, propertyGetter pGet)
+    :  m_objInstance(objInstance), getterMethod(pGet)
     {}
-    operator _Prop_t() { return (m_objInstance.*m_pmGet)(); }
+    operator PropertyType() { return (m_objInstance.*getterMethod)(); }
 };
 
 #endif
